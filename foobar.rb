23dabc19067beb41d5273fe97c48f1ef477c68a1
Thomas Bates <tbates@ymail.com>
2017-09-13 23:40:06 -0700

    Completed homework 1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.collect{|string| string.to_i + 2}.select{|num| num % 2 == 0 and num <= 10}.uniq.inject(0){|sum,x| sum + x}
   end
 end
 
